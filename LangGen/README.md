f5db243f7b8993bb0c5a9fe2ba1e9c660a645f8e
Simon <Shazuli@users.noreply.github.com>
2019-10-21 17:27:44 +0200

    Code efficiency improvement + Item/block filter + es_es
    Removed redundant variables, added block/item filter and added Spanish translation.

--- LangGen/README.md
+++ LangGen/README.md
@@ -1,2 +1,2 @@
 # LangGen
-Utility program to automatically make large amounts of item translations from a json template for my Minecraft mods, written in JavaScript.
+Utility program to automatically make large amounts of item translations from a json template for my Minecraft mods, written in JavaScript in [Node.js](https://nodejs.org/en/) environment.
--- LangGen/index.js
+++ LangGen/index.js
@@ -2,54 +2,58 @@ const fs = require('fs');
 
 
 
-const lang = 'de_de.json'; // Change this to the desired language file name.
+const lang      = 'en_us.json'; // Change this to the desired language file name.
+const spacing   = 2;            // The desired spacing in the output file.
+const separated = true;         // If the blocks and items should be separated or mixed in the output file.
 
-const keywords = [ // Add more keywords if this is not up to date.
-    "ingot",
-    "nugget",
-    "dust",
-    "dustsmall",
-    "plate"
-];
-
-fs.readFile("input/" + lang, (err,data) => {
+fs.readFile('input/' + lang, (err,data) => {
     if (err) throw err;
-    let json = JSON.parse(data);
+    const json = JSON.parse(data);
 
-    let JSONObjects = [];
+    const JSONObject = [{},{}];
 
     json.elements.forEach(element => {
 
-        for (var i in keywords) {
+        for (var i in json.types) {
 
             var shouldSkip = false;
-            if (element.filter) {
-                for (var j=0; j<element.filter.length;j++) {
-                    if (element.filter[j] == keywords[i] || element.filter[j] == "none") {
+            if (element.filter)
+                for (var j=0;j<element.filter.length;j++) {
+                    if (element.filter[j] == Object.keys(json.types[i])[0] || element.filter[j] == "none") {
                         shouldSkip = true;
                         break;
                     }
-                }    
-            }
+                }
+            
 
             if (shouldSkip) continue;
 
-            var translation = json.types[i][keywords[i]];
-            var keyword = json.domain + "." + Object.keys(element)[0] + "_" + keywords[i];
+            var translation = json.types[i][Object.keys(json.types[i])[0]];
+            var keyword = json.domain + "." + Object.keys(element)[0] + "_" + Object.keys(json.types[i])[0];
 
             translation = translation.replace("%s", element[Object.keys(element)[0]]);
 
-            //var obj = "\"" + keyword + "\" : \"" + translation + "\"";
-            var obj = { ["item." + keyword]: translation };
-            console.log(JSON.stringify(obj));
-            JSONObjects.push(obj);
+
+            var y = json.types[i].isBlock == true;
+
+            var t = y ? 'block.' : 'item.';
+
+            console.log("\"" + t + keyword + "\" : \"" + translation + "\"");
+            if (separated)
+                JSONObject[y ? 1 : 0][t + keyword] = translation;
+            else
+                JSONObject[0][t + keyword] = translation;
         }
     });
-
-    var jsonVar = {};
-    for (var i=0;i<JSONObjects.length;i++) {
-        jsonVar[Object.keys(JSONObjects[i])] = JSONObjects[i][Object.keys(JSONObjects[i])];
-    }
-    fs.writeFileSync("output/" + lang, JSON.stringify(jsonVar, null, 2));
+    fs.writeFileSync("output/" + lang, JSON.stringify(merge(JSONObject[0],JSONObject[1]), null, spacing));
     console.log("Wrote to output/" + lang + ".");
 });
+
+
+function merge(a,b)
+{
+    const r = {};
+    Object.keys(a).forEach(key => r[key] = a[key]);
+    Object.keys(b).forEach(key => r[key] = b[key]);
+    return r;
+}
