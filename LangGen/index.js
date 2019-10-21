127c6a37233e06c9963920e48a1be44f06c42915
Simon <Shazuli@users.noreply.github.com>
2019-10-21 22:04:07 +0200

    Flag mechanic + write-to-disk check + description
    Added flag mechanic to allow for automatic sorting for groups in the output file, added so it only writes to disk if it sees any changes, added description.
    Should be the last commit to the program unless I spot a code improvement (probably tomorrow), otherwise the program is done and will only be seen as an feature creep.

--- LangGen/index.js
+++ LangGen/index.js
@@ -1,22 +1,32 @@
+/**
+ * A tool to generate language translations for blocks and items for my Minecraft mods
+ * with similar names from a Json file blueprint. A high degree of customization of the Json
+ * is possible to generate a large amount of different translations with language keys in specified order
+ * ready for file transfer or manually copy-paste into desired language file, written in JavaScript in Node.js.
+ *
+ * By Simon
+ */
+
 const fs = require('fs');
 
 
-
-const lang      = 'en_us.json'; // Change this to the desired language file name.
+const lang      = 'de_de.json'; // Change this to the desired language file name in input/.
 const spacing   = 2;            // The desired spacing in the output file.
-const separated = true;         // If the blocks and items should be separated or mixed in the output file.
+const separated = true;         // If the flagged types should be separated or mixed in the output file.
+
+//////////////////////// Don't edit below this ////////////////////////
 
 fs.readFile('input/' + lang, (err,data) => {
     if (err) throw err;
     const json = JSON.parse(data);
 
-    const JSONObject = [{},{}];
+    let JSONObjects = [{}];
 
     json.elements.forEach(element => {
 
-        for (var i in json.types) {
+        for (let i in json.types) {
 
-            var shouldSkip = false;
+            let shouldSkip = false;
             if (element.filter)
                 for (var j=0;j<element.filter.length;j++) {
                     if (element.filter[j] == Object.keys(json.types[i])[0] || element.filter[j] == "none") {
@@ -28,32 +38,47 @@ fs.readFile('input/' + lang, (err,data) => {
 
             if (shouldSkip) continue;
 
-            var translation = json.types[i][Object.keys(json.types[i])[0]];
-            var keyword = json.domain + "." + Object.keys(element)[0] + "_" + Object.keys(json.types[i])[0];
+            let translation = json.types[i][Object.keys(json.types[i])[0]];
+            let keyword = json.domain + "." + Object.keys(element)[0] + "_" + Object.keys(json.types[i])[0];
 
             translation = translation.replace("%s", element[Object.keys(element)[0]]);
 
-
-            var y = json.types[i].isBlock == true;
-
-            var t = y ? 'block.' : 'item.';
+            let t = json.types[i].flag == "block" ? 'block.' : 'item.';
 
             console.log("\"" + t + keyword + "\" : \"" + translation + "\"");
-            if (separated)
-                JSONObject[y ? 1 : 0][t + keyword] = translation;
+            if (separated) {
+                let index = 0;
+                if (json.types[i].flag) {
+                    index = json.types[i].flag;
+                    if (JSONObjects[index] == undefined) {
+                        JSONObjects[index] = {};
+                    }
+                }                
+                JSONObjects[index][t + keyword] = translation;
+            }
             else
-                JSONObject[0][t + keyword] = translation;
+                JSONObjects[0][t + keyword] = translation;
         }
     });
-    fs.writeFileSync("output/" + lang, JSON.stringify(merge(JSONObject[0],JSONObject[1]), null, spacing));
-    console.log("Wrote to output/" + lang + ".");
+
+    const newJson = merge(JSONObjects);
+
+    fs.readFile('output/' + lang, (err, data) => {
+
+        const text = JSON.stringify(newJson, null, spacing);
+
+        if (!err)
+            if (text == data.toString()) {
+                console.error("No changes to output/" + lang);
+                return;
+            }
+        
+        fs.writeFileSync("output/" + lang, text);
+        console.log("Wrote to output/" + lang);
+    });
 });
 
-
-function merge(a,b)
+function merge(a)
 {
-    const r = {};
-    Object.keys(a).forEach(key => r[key] = a[key]);
-    Object.keys(b).forEach(key => r[key] = b[key]);
-    return r;
+    const r = {};Object.keys(a).forEach(j => Object.keys(a[j]).forEach(key => r[key] = a[j][key]));return r;
 }
